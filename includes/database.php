f3a5616c30a6d14e5efa15d06167100babfe1d64
cobain325 <cobain325@gmail.com>
2023-05-25 00:32:58 +1200

    ffs

--- includes/database.php
+++ includes/database.php
@@ -3,15 +3,13 @@ $servername = "bit608-server.mysql.database.azure.com";
 $username = "haffxpeibm";
 $password = "H15N8Z0FFM4CUY6M";
 
-
 // Create connection
 try {
     $conn = mysqli_init();
-    mysqli_ssl_set($conn, NULL, NULL, "ssl.pem", NULL, NULL);
+    mysqli_ssl_set($conn, NULL, NULL, __DIR__ .'/ssl.pem', NULL, NULL);
     mysqli_real_connect($conn, $servername, $username, $password, 'motueka', 3306, MYSQLI_CLIENT_SSL);
 } catch (Exception $e) {
     echo "Service unavailable \n";
-    print($e);
     var_dump($e);
     exit;
 }
