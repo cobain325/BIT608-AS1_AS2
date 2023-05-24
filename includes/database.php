61fb36a1ea90e79cd6913354d5d25591aca1df8b
cobain325 <cobain325@gmail.com>
2023-05-25 00:11:07 +1200

    db

--- includes/database.php
+++ includes/database.php
@@ -3,13 +3,16 @@ $servername = "bit608-server.mysql.database.azure.com";
 $username = "haffxpeibm";
 $password = "H15N8Z0FFM4CUY6M";
 
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, "/ssl.pem", NULL, NULL);
+
 // Create connection
 try {
-    $conn = new mysqli($servername, $username, $password, 'motueka');
-} catch (Exception $e ) {
+    mysqli_real_connect($conn, $servername, $username, $password, 'motueka', 3306, MYSQLI_CLIENT_SSL);
+} catch (Exception $e) {
     echo "Service unavailable";
     exit;
 }
-if(!isset($_SESSION['alertList']["Database Connected Successfully"])){
+if (!isset($_SESSION['alertList']["Database Connected Successfully"])) {
     $_SESSION['alertList']["Database Connected Successfully"] = array("type" => "success", "viewed" => 0);
-} 
+}
