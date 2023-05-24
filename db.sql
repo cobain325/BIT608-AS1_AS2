51f2093c61b00ed0ead77f0b4dc99ae7cb4873e6
cobain325 <cobain325@gmail.com>
2023-05-24 23:53:55 +1200

    s

--- db.sql
+++ db.sql
@@ -87,7 +87,7 @@ CREATE TABLE `customer` (
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '.'
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
