05d9b008f3204ff0903c2a8346c35d1da30aa5bc
cobain325 <cobain325@gmail.com>
2023-05-26 14:30:13 +1200

    Rooms page data basic display.

--- views/rooms.php
+++ views/rooms.php
@@ -1,7 +1,22 @@
+<div class="card-header">
+    Our Rooms:
+</div>
+<ul class="list-group">
 <?php
     global $conn;
     $rooms = $conn->query("SELECT * FROM room");
     foreach($rooms as $room){
-        echo var_dump($room);
+        echo "
+        <li class=\"list-group-item\">
+            <div class=\"row\">
+                <div class=\"col-12 col-lg-9\">
+                    <p>Name:" . $room['roomname'] . "</p>
+                    <p>Description:" . $room['description'] . "</p>
+                    <p>Room Type:" . $room['roomtype'] . "</p>
+                    <p>Beds:" . $room['beds'] . "</p>
+                </div>
+            </div>
+        </li>";
     }
 ?>
+</ul>
