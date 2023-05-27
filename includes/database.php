3a0f616bbe238d4cc90d0e3525477ec55e0d3dc7
cobain325 <cobain325@gmail.com>
2023-05-27 14:42:50 +1200

    db fix

--- includes/database.php
+++ includes/database.php
@@ -2,14 +2,14 @@
 $host = "bit608-server.mysql.database.azure.com";
 $username = "haffxpeibm";
 $password = "H15N8Z0FFM4CUY6M$";
-$ca_certificate = "/ssl.pem";
+$ca_certificate = __DIR__ . "/ssl.pem";
 $database = "motueka";
 $port = 3306;
 
 // Create connection
 try {
     $conn = mysqli_init();
-    mysqli_ssl_set($conn, NULL, NULL, __DIR__ . $ca_certificate, NULL, NULL);
+    mysqli_ssl_set($conn, NULL, NULL, $ca_certificate, NULL, NULL);
     mysqli_real_connect($conn, $servername, $username, $password, $database, $port, MYSQLI_CLIENT_SSL);
 } catch (Exception $e) {
     echo "Service unavailable \n";
